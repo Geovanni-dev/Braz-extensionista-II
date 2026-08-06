992081254ce1743309d51e9f462f20c82a9a466f
Geovanni-dev <geovanni3344@outlook.com>
2026-08-06 13:27:43 -0300

    chore: initial project setup with Prisma, ESLint, Prettier and Express server

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+//--- imports
+import 'dotenv/config';
+import express, { type Express } from 'express';
+import cors from 'cors';
+
+class Server {
+  public app: Express;
+
+  constructor() {
+    if (process.env.NODE_ENV === 'production') {
+      console.log = () => {}; // disable console.log in production
+    }
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  private middlewares(): void {
+    this.app.use(cors());
+    this.app.use(express.json());
+  }
+
+  private routes(): void {
+    this.app.get('/', (_req, res) => {
+      res.send('Hello World!');
+    });
+  }
+}
+
+export const app = new Server().app;
