b6062a59d232e7c452eb746bcd71d1a99b8c4b6c
Geovanni-dev <geovanni3344@outlook.com>
2026-08-28 00:15:23 -0300

    feat: consolidate aluno seeding logic into seed.ts and remove redundant file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,12 +5,21 @@ import { z } from 'zod';
 
 const prisma = new PrismaClient();
 
+// zod schemas
+
 const chaveSchema = z.object({
   CHAVE_MEIRES: z.string(),
   CHAVE_ARIANE: z.string(),
   CHAVE_WESLANE: z.string(),
   CHAVE_LUCIANNE: z.string(),
   CHAVE_HELITA: z.string(),
+});
+
+const alunoSchema = z.object({
+  nome: z.string().min(3),
+  email: z.string().email(),
+  senha: z.string().min(6),
+  codigo: z.string(),
 });
 
 // Seed the professores
@@ -86,6 +95,37 @@ async function seedDisciplinas() {
     return logger.info('Disciplinas criadas/atualizadas com sucesso.');
   } catch (error) {
     logger.error(error, 'Erro ao criar/atualizar disciplina.');
+  }
+}
+
+async function seedAluno() {
+  try {
+    const { nome, email, senha } = alunoSchema.parse({
+      nome: process.env.ALUNO_NAME,
+      email: process.env.ALUNO_EMAIL,
+      senha: process.env.ALUNO_PASSWORD,
+    });
+    logger.info('Dados validados pelo esquema do zod:');
+    const passwordHash = await bcrypt.hash(senha, 10);
+
+    const aluno = await prisma.aluno.upsert({
+      where: { email },
+      update: { nome, senha: passwordHash },
+      create: {
+        nome,
+        email,
+        senha: passwordHash,
+      },
+    });
+    logger.info(
+      `Usuário criado/atualizado com sucesso: ${aluno.nome} (${aluno.email})`,
+    );
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      logger.error('Erro de validação do zod:');
+    } else {
+      logger.error('Erro ao criar o usuário:');
+    }
   } finally {
     await prisma.$disconnect();
   }
@@ -93,3 +133,4 @@ async function seedDisciplinas() {
 
 await seedProfessors();
 await seedDisciplinas();
+await seedAluno();
